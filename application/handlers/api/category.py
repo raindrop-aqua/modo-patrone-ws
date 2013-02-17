549711cb25a79cd1df706099f659bdaa985fd78f
raindrop-aqua <raindrop.aqua@gmail.com>
2013-02-18 04:39:30 +0900

    init_category commented.

--- application/handlers/api/category.py
+++ application/handlers/api/category.py
@@ -5,14 +5,13 @@ from application.services.facade.category import CategoryFacade
 
 category_api = Module(__name__)
 
-@category_api.route("/category/init")
-def init_category():
-    res = CategoryFacade.init()
-    return jsonify(res)
-
 
 @category_api.route("/category/list")
 def list_category():
     res = CategoryFacade.list()
     return jsonify(res)
 
+# @category_api.route("/category/init")
+# def init_category():
+#     res = CategoryFacade.init()
+#     return jsonify(res)
